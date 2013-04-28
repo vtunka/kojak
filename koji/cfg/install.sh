f036f16b0f679818385dd1b8574080de56a52373
Salim Badakhchani <sal@redhat.com>
2013-04-28 22:07:12 +0200

    Updated install.sh

--- koji/cfg/install.sh
+++ koji/cfg/install.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
+# Install the required rpm binaries
 yum -y install httpd mod_ssl postgresql-server mod_wsgi mock setarch rpm-build createrepo koji koji-builder koji-hub koji-hub-plugins koji-utils koji-vm koji-web
 
+# Added the koji user and set the passwd
 useradd koji
+echo koji | passwd koji --stdin
 
-make-koji-certs
+# Create the koji server, component and user SSL certs
+../make-koji-certs
 
-
+# Initialise the postgresql database
 postgresql-setup initdb
 
+# Backup the original configuration and copy other the updated files over.
 mv /etc/bashrc /etc/bashrc.orig && cp etc/bashrc /etc/bashrc
 mv /etc/koji.conf /etc/koji.confi.orig && cp etc/koji.conf /etc/koji.conf
 mv /etc/profile /etc/profile.orig && cp etc/profile /etc/profile
@@ -27,22 +32,22 @@ chown postgres:postgres /var/lib/pgsql/data/pg_hba.conf
 chown postgres:postgres /var/lib/pgsql/data/postgresql.conf
 chown koji:apache /home/koji/.koji/config
 
+# Bring the datatbase up
 chkconfig postgresql on
 service postgresql start
 
-
+# Create the koji table spaces
 su - postgres
 createuser koji
 createdb -O koji koji
 logout
 su - koji
 psql koji koji < /usr/share/doc/koji*/docs/schema.sql
-/q
-
 insert into users (name, status, usertype) values ('kojiadmin', 0, 0);
 insert into user_perms (user_id, perm_id, creator_id) values (1, 1, 1);
+/q
 
-# Create koji build dirs
+# Create koji build directories
 cd /mnt
 mkdir koji
 cd koji
