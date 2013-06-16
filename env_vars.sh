d4b04f0882a7a382bf5abda5bb917f160354deb7
Salim Badakhchani <sal@redhat.com>
2013-06-16 21:19:53 +0200

    Updated env_vars.sh

--- env_vars.sh
+++ env_vars.sh
@@ -21,7 +21,7 @@
 
 
 VMNAME="Fedora-18-x86_64-DVD"
-VMHOME="/mnt/media/USB0"
+VMHOME="/var/lib/libvirt/images/"
 TMPDIR="/home/kojak"
 DIST="RedHat/Fedora/18/0/x86_64"
 
@@ -30,7 +30,6 @@ KS_ISO="Fedora-18-x86_64-DVD.iso"
 KS_CFG="Fedora-18-x86_64.cfg"
 
 MNTDIR="${TMPDIR}/mnt/${DIST}"
-OPTDIR="${TMPDIR}/opt/${DIST}"
 CFGDIR="${TMPDIR}/cfg/${DIST}"
 BLDDIR="${TMPDIR}/bld/${DIST}"
 ISODIR="${TMPDIR}/iso/${DIST}"
